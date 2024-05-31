89a383a704bd0536432f65c745070d1b955e6239
mraihanhatim <raihan04042001@gmail.com>
2024-05-31 22:55:43 +0700

    edit index script and style

--- script.js
+++ script.js
@@ -2,16 +2,8 @@ window.onscroll = function () {
   scrollFunction();
 };
 
-function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("navbar").style.backgroundColor = "rgb(25, 25, 25)"; /* Warna baru ketika di-scroll */
-  } else {
-    document.getElementById("navbar").style.backgroundColor = "rgb(25, 25, 25)"; /* Warna default ketika tidak di-scroll */
-  }
-}
-
 new TypeIt(".ntmy", {
-  speed: 100,
+  speed: 350,
   waitUntilVisible: true,
 }).go();
 
